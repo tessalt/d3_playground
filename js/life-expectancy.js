2a293893e187c29a06ffa1d9dc8bda47eeb5937f
Tessa Thornton <tessathornton@gmail.com>
2013-07-25 17:26:18 -0500

    pull in countries dynamically and assign colours. closes #3

--- js/life-expectancy.js
+++ js/life-expectancy.js
@@ -5,24 +5,38 @@ var w = viewportWidth,
     h = viewportHeight - 100,
     margin = 50;
 
+var color = d3.scale.category10();
 
 var parseDate = d3.time.format("%Y").parse;
 
+
+
 d3.tsv("data/life-expectancy.tsv", function(error, data){
+
+  color.domain(d3.keys(data[0]).filter(function(key) { return key !== "year"; }));
  
   data.forEach(function(d){
     d.date = parseDate(d.year);
-    d.canada = d.canada;
-    d.australia = d.australia;
-    d.us = d.us;
+  });
+
+  var countries = color.domain().map(function(name) {
+    return {
+      name: name,
+      values: data.map(function(d) {
+        return {date: d.date, age: +d[name]};
+      })
+    };
   });
 
   var yScale = d3.scale.linear()
-    .domain([d3.max(data.map(function(d) { return d.australia; })), d3.min(data.map(function(d) { return d.us; }))])
+    .domain([
+      d3.max(countries, function(c) { return d3.max(c.values, function(v) { return v.age; }); }),
+      d3.min(countries, function(c) { return d3.min(c.values, function(v) { return v.age; }); })
+    ])
     .range([0 + margin, h - margin]);
 
   var xScale = d3.time.scale()
-    .domain([d3.min(data.map(function(d) { return d.date; })), d3.max(data.map(function(d) { return d.date; }))])
+    .domain(d3.extent(data, function(d) { return d.date; }))
     .range([0 + margin, w - margin]);
 
   var yAxis = d3.svg.axis()
@@ -58,33 +72,26 @@ d3.tsv("data/life-expectancy.tsv", function(error, data){
     .attr("stroke", "none")
     .attr("fill", "none");
 
-  var canG = svg.append("g"),
-      ausG = svg.append("g"),
-      usG = svg.append("g");
+  var country = svg.selectAll(".country")
+    .data(countries)
+    .enter()
+    .append("g")
+    .attr("class", "country");
 
-  var canLine = d3.svg.line()
-    .x(function(d){return xScale(d.date);})
-    .y(function(d){return 1 * yScale(d.canada);})
 
-  var ausLine = d3.svg.line()
-    .x(function(d){return xScale(d.date);})
-    .y(function(d){return 1 * yScale(d.australia);})
+var line = d3.svg.line()
+  .interpolate("basis")
+  .x(function(d){return xScale(d.date);})
+  .y(function(d){return 1 * yScale(d.age);});
 
-  var usLine = d3.svg.line()
-    .x(function(d){return xScale(d.date);})
-    .y(function(d){return 1 * yScale(d.us);})
-
-  canG.append("path")
-    .attr("class", "can plot")
-    .attr("d", canLine(data));
-
-  ausG.append("path")
-    .attr("class", "aus plot")
-    .attr("d", ausLine(data));
-
-  usG.append("path")
-    .attr("class", "usa plot")
-    .attr("d", usLine(data));
+  country.append("path")
+    .attr("class", "line")
+    .attr("d", function(d){
+      return line(d.values);
+    })
+    .style("stroke", function(d){
+      return color(d.name);
+    });
 
 
 });
