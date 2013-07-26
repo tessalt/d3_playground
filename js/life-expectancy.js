ecc121ec3711229fbf532559cab067ae49f7aa51
Tessa Thornton <tessathornton@gmail.com>
2013-07-25 19:10:28 -0500

    comment code

--- js/life-expectancy.js
+++ js/life-expectancy.js
@@ -1,25 +1,58 @@
-var viewportWidth  = document.documentElement.clientWidth
-  , viewportHeight = document.documentElement.clientHeight
-
-var w = viewportWidth,
+// sizing vars
+var viewportWidth  = document.documentElement.clientWidth, 
+    viewportHeight = document.documentElement.clientHeight,
+    w = viewportWidth,
     h = viewportHeight - 100,
     margin = 50;
 
-var color = d3.scale.category10();
+// create svg element
+var svg = d3.select(".container")
+  .append("svg")
+  .attr("width", w)
+  .attr("height", h);
 
+// set scales 
+var colorScale = d3.scale.category10();
+
+var xScale = d3.time.scale()
+  .range([0 + margin, w - margin]);
+
+var yScale = d3.scale.linear() 
+  .range([0 + margin, h - margin]);
+
+// set axes
+var xAxis = d3.svg.axis()
+  .scale(xScale)
+  .orient("bottom");
+
+var yAxis = d3.svg.axis()
+  .scale(yScale)
+  .tickPadding(margin / 4)
+  .tickSize(-w + margin * 2, 0)
+  .orient("left");
+
+// define line method
+var line = d3.svg.line()
+  .interpolate("basis")
+  .x(function(d){return xScale(d.date);})
+  .y(function(d){return 1 * yScale(d.age);});
+
+// define parse data method
 var parseDate = d3.time.format("%Y").parse;
 
-
-
+// load and parse data
 d3.tsv("data/life-expectancy.tsv", function(error, data){
-
-  color.domain(d3.keys(data[0]).filter(function(key) { return key !== "year"; }));
+  
+  // set domain for color scale based on values for countries
+  colorScale.domain(d3.keys(data[0]).filter(function(key) { return key !== "year"; }));
  
-  data.forEach(function(d){
+ // parse year values
+  data.forEach(function(d){  
     d.date = parseDate(d.year);
   });
 
-  var countries = color.domain().map(function(name) {
+  // map data from countries to object
+  var countries = colorScale.domain().map(function(name) {
     return {
       name: name,
       values: data.map(function(d) {
@@ -28,69 +61,44 @@ d3.tsv("data/life-expectancy.tsv", function(error, data){
     };
   });
 
-  var yScale = d3.scale.linear()
-    .domain([
-      d3.max(countries, function(c) { return d3.max(c.values, function(v) { return v.age; }); }),
-      d3.min(countries, function(c) { return d3.min(c.values, function(v) { return v.age; }); })
-    ])
-    .range([0 + margin, h - margin]);
+  // set y scale domain based on range of ages
+  yScale.domain([
+    d3.max(countries, function(c) { return d3.max(c.values, function(v) { return v.age; }); }),
+     d3.min(countries, function(c) { return d3.min(c.values, function(v) { return v.age; }); }) 
+  ]);
 
-  var xScale = d3.time.scale()
-    .domain(d3.extent(data, function(d) { return d.date; }))
-    .range([0 + margin, w - margin]);
-
-  var yAxis = d3.svg.axis()
-    .scale(yScale)
-    .tickPadding(margin / 4)
-    .tickSize(-w + margin * 2, 0)
-    .orient("left");
-
-  var xAxis = d3.svg.axis()
-    .scale(xScale)
-    .ticks(data.length/4)
-    .orient("bottom");
-
-  var svg = d3.select(".container")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h);
-
+  // set x scale domain based on rage of years
+  xScale.domain(
+    d3.extent(data, function(d) { return d.date; })
+  );
+  
+  // create y axis
   svg.append("g")
     .attr("class", "axis")
     .attr("transform", "translate(" + margin + ",0)")
     .call(yAxis);
 
+ // create x axis   
   svg.append("g")
     .attr("class", "axis")
     .call(xAxis)
     .attr("transform", "translate(0," + (h - margin) + ")");
 
-  d3.selectAll(".axis text")
-    .attr("fill", "#ecf0f1");
-
-  d3.selectAll(".axis path, .axis path")
-    .attr("stroke", "none")
-    .attr("fill", "none");
-
+  // create paths for countries
   var country = svg.selectAll(".country")
     .data(countries)
     .enter()
     .append("g")
     .attr("class", "country");
 
-
-var line = d3.svg.line()
-  .interpolate("basis")
-  .x(function(d){return xScale(d.date);})
-  .y(function(d){return 1 * yScale(d.age);});
-
+  // plot paths to data, style paths with colour scale
   country.append("path")
     .attr("class", "line")
     .attr("d", function(d){
       return line(d.values);
     })
     .style("stroke", function(d){
-      return color(d.name);
+      return colorScale(d.name);
     });
 
 
